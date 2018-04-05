ba36c30bd1662b218b9e279d1ed1e02038ff9b58
yuto kondo <y.kondo@pepabo.com>
2018-04-06 00:25:00 +0900

    ボタンを押したら出勤

--- poti.js
+++ poti.js
@@ -4,5 +4,10 @@ const Gpio = require('onoff').Gpio;
 var button = new Gpio(18, 'on', 'both');
 
 button.watch(function(err, value) {
+  if(value == 1){
   console.log("Button has been pressed");
+  const proc = require('child_process').execSync;
+  var result = proc('~/go/kinnosuke-clocking-cli/kinnosuke-clocking-cli');
+  console.log("Done");
+  }
 });
